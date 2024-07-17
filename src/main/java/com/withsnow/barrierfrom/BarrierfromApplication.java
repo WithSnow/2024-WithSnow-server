fcbb101174c81c642fb40949c96029b03e0e071a
egg-z1 <kimfreedom25@gmail.com>
2024-07-17 11:14:17 +0900

    Feat: 초기세팅

---
+++ src/main/java/com/withsnow/barrierfrom/BarrierfromApplication.java
@@ -0,0 +1,13 @@
+package com.withsnow.barrierfrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierfromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrierfromApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/withsnow/barrierfrom/BarrierfromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withsnow.barrierfrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrierfromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
