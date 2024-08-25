f9daa582ad7be5f3d492bfb797fe16f9cb36db9e
egg-z1 <kimfreedom25@gmail.com>
2024-08-25 16:15:14 +0900

    feat: 검색하기 단일 삭제, 전체 삭제 추가 (#13)

--- src/main/java/com/withsnow/barrierfrom/search/SearchHistoryController.java
+++ src/main/java/com/withsnow/barrierfrom/search/SearchHistoryController.java
@@ -2,9 +2,7 @@ package com.withsnow.barrierfrom.search;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +13,27 @@ public class SearchHistoryController {
     @Autowired
     private SearchHistoryRepository searchHistoryRepository;
 
-    @GetMapping
+    @GetMapping //검색기록 조회하기
     public ResponseEntity<List<SearchHistory>> getSearchHistory() {
         List<SearchHistory> searchHistoryList = searchHistoryRepository.findAll();
         return ResponseEntity.ok(searchHistoryList);
+    }
+
+    // 검색 기록 단일 삭제하기
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSearchHistory(@PathVariable Long id) {
+        if (searchHistoryRepository.existsById(id)) {
+            searchHistoryRepository.deleteById(id);
+            return ResponseEntity.noContent().build(); // 성공적으로 삭제했음을 반환
+        } else {
+            return ResponseEntity.notFound().build(); // 삭제할 기록이 없음을 반환
+        }
+    }
+
+    // 검색 기록 전체 삭제하기
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAllSearchHistory() {
+        searchHistoryRepository.deleteAll();
+        return ResponseEntity.noContent().build(); // 성공적으로 전체 삭제했음을 반환
     }
 }
